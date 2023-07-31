819cb73bb74fd0951ad3cd14f4c43275c6ea219e
ha-ny <hany01.dev@gmail.com>
2023-07-31 16:33:01 +0900

    setting

---
+++ BookShelfCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfCollectionViewCell.swift
+//  BookShelf
+//
+//  Created by 김하은 on 2023/07/31.
+//
+
+import UIKit
+
+class BookShelfCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
